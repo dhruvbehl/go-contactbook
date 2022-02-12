8dabc1b2382defc6be9ddd47afc6c3ee38879d2f
dhruv behl <dhruvbhl@gmail.com>
2022-02-13 01:18:17 +0530

    Adding swagger support & vendor

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
+	_ "github.com/dhruvbehl/address-book/docs"
 	"github.com/dhruvbehl/address-book/pkg/router"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Contact Book
+// @version 1.0
+// @description Contact book written in golang with the purpose of learning golang rest framework gin
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Dhruv Behl
+// @contact.email dhruvbhl@gmail.com
+
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 func main() {
 	address := flag.String("address", ":8080", "http address of the server")
 	flag.Parse()
 
 	engine := gin.Default()
+	url := ginSwagger.URL(fmt.Sprintf("%s/swagger/doc.json", *address))
+  	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	engine.GET("/contacts", router.GetAllContactHandler)
 	engine.GET("/contact/:id", router.GetContactByIdHandler)
@@ -19,5 +37,7 @@ func main() {
 	engine.PUT("/contact/:id", router.UpdateContactHandler)
 	engine.DELETE("/contact/:id", router.DeleteContactByIdHandler)
 
-	engine.Run(*address)
+	if err := engine.Run(*address); err != nil {
+		log.Fatal().Err(err).Msg("unable to start the server")
+	}
 }
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -8,6 +8,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetAllContact godoc
+// @id GetAllContact
+// @Summary Show all the contacts stored on the server
+// @Description Show all the contacts stored on the server
+// @Accept */*
+// @Produce json
+// @Success 200 {array} map[string]api.Contact{}
+// Failure 404 {object} map[string]interface{} "Not Found"
+// @Router /contacts [get]
 func GetAllContactHandler(ctx *gin.Context) {
 	response, err := contact.GetAllContact()
 	if err != nil {
@@ -17,6 +26,16 @@ func GetAllContactHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"data": response})
 }
 
+// GetContactById godoc
+// @id GetContactById
+// @Summary Show contact by id
+// @Description Show contact by id
+// @Param id path string true "Contact ID"
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]api.Contact{}
+// Failure 404 {object} map[string]interface{} "Not Found"
+// @Router /contact/{id} [get]
 func GetContactByIdHandler(ctx *gin.Context) {
 	id := ctx.Param(("id"))
 	response, err := contact.GetContactById(id)
@@ -27,6 +46,16 @@ func GetContactByIdHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"data": response})
 }
 
+// GetContactById godoc
+// @id DeleteContactById
+// @Summary Delete contact by id
+// @Description Delete contact by id "Contact ID"
+// @Param id path string true "Contact ID"
+// @Accept */*
+// @Produce json
+// @Success 200
+// Failure 404 {object} map[string]interface{} "Not Found"
+// @Router /contact/{id} [delete]
 func DeleteContactByIdHandler(ctx *gin.Context) {
 	id := ctx.Param("id")
 	err := contact.DeleteContactById(id)
@@ -37,6 +66,17 @@ func DeleteContactByIdHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, nil)
 }
 
+// CreateContact godoc
+// @id CreateContact
+// @Summary Create contact
+// @Description Create contact
+// @Param contactRequest body api.Contact true "Contact Request Body"
+// @Accept json
+// @Produce json
+// @Success 201 {object} map[string]api.Contact{}
+// Failure 409 {object} map[string]interface{} Conflict
+// Failure 500 {object} map[string]interface{} "Internal Server Error"
+// @Router /contact [post]
 func CreateContactHandler(ctx *gin.Context) {
 	var c api.Contact
 	if err := ctx.Bind(&c); err != nil {
@@ -51,6 +91,18 @@ func CreateContactHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, gin.H{"data": response})
 }
 
+// UpdateContact godoc
+// @id UpdateContact
+// @Summary Update contact
+// @Description Update contact
+// @Param id path string true "Contact ID"
+// @Param contactRequest body api.Contact true "Contact Request Body"
+// @Accept json
+// @Produce json
+// @Success 201 {object} map[string]api.Contact{}
+// Failure 500 {object} map[string]interface{} "Internal Server Error"
+// Failure 404 {object} map[string]interface{} "Not Found"
+// @Router /contact/{id} [put]
 func UpdateContactHandler(ctx *gin.Context) {
 	var c api.Contact
 	id := ctx.Param("id")
